5898c92be9e45d704adbf6526eeb8098b2ed33ae
Luc Saffre <luc.saffre@gmail.com>
2019-11-01 15:01:53 +0200

    added intersphinx url

--- lino_react/__init__.py
+++ lino_react/__init__.py
@@ -17,4 +17,5 @@ __version__ = SETUP_INFO['version']
 
 # intersphinx_urls = dict(docs="http://react.lino-framework.org")
 srcref_url = 'https://github.com/lino-framework/react/blob/master/%s'
-# doc_trees = ['docs']
+doc_trees = ['docs']
+intersphinx_urls = dict(docs="http://react.lino-framework.org")
